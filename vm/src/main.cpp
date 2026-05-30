5fce2da7c9b00a417164086e707d02f06e2b1b77
ruanazevedo <ruannzeroh@gmail.com>
2026-05-30 00:40:03 -0300

    refactoring include dir

--- vm/src/main.cpp
+++ vm/src/main.cpp
@@ -1,6 +1,6 @@
 #include <filesystem>
 
-#include "../includes/vm.h"
+#include "vm.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
--- vm/src/vm.cpp
+++ vm/src/vm.cpp
@@ -1,4 +1,4 @@
-#include "../includes/vm.h"
+#include "vm.h"
 
 VirtualMachine::VirtualMachine(const char* binFile, int flag) {
     this->flag = flag;
